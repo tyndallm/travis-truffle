a08e579172ec0ab4e116a8d19127c823dff1e8bf
Matt Tyndall <matt.tyndall@consensys.net>
2017-07-18 10:25:44 -0400

    Add true test

--- test/metacoin.js
+++ test/metacoin.js
@@ -60,4 +60,7 @@ contract('MetaCoin', function(accounts) {
       assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
     });
   });
+  it("should return true", function() {
+    assert.equal(true, true, "This must return true");
+  });
 });
